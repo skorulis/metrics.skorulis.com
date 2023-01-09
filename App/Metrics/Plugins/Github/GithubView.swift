93aebb343e5171a82bec06b4b33663ee5ca4a5d9
Alex Skorulis <skorulis@gmail.com>
2023-01-09 20:15:29 +1100

    Improve the github view

--- App/Metrics/Plugins/Github/GithubView.swift
+++ App/Metrics/Plugins/Github/GithubView.swift
@@ -14,7 +14,37 @@ struct GithubView {
 extension GithubView: View {
     
     var body: some View {
-        Text("Github")
+        VStack {
+            Text("Commits: \(totalCommits)")
+            ForEach(languageLines, id: \.0) { line in
+                Text("\(line.0): \(line.1)")
+            }
+        }
+    }
+    
+    var totalCommits: Int {
+        let commits = data.values.map { repo in
+            return repo.diff?.commitCount ?? 0
+        }
+        return commits.reduce(0, +)
+    }
+    
+    var languageLines: [(String, Int)] {
+        var dict: [String: Int] = [:]
+        for repo in data.values {
+            for (lang, bytes) in (repo.diff?.languageBytes ?? [:]) {
+                let total = (dict[lang] ?? 0) + bytes
+                dict[lang] = total
+            }
+        }
+        var ret: [(String, Int)] = []
+        for (key, value) in dict {
+            ret.append((key, value))
+        }
+        
+        return ret.filter { $0.1 != 0}.sorted { s1, s2 in
+            return s1.1 > s2.1
+        }
     }
 }
 
