e7f9db6158f78941425183ebc6ab3d989b5de1c7
Alex Skorulis <skorulis@gmail.com>
2023-01-16 17:49:52 +1100

    Add iOS app

---
+++ App/MetricsIOS/MetricsIOSApp.swift
@@ -0,0 +1,12 @@
+//Created by Alexander Skorulis on 16/1/2023.
+
+import SwiftUI
+
+@main
+struct MetricsIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
